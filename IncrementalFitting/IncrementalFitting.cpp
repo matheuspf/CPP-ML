b67ddbc536e4e03c64e08ba3f68c0f030ee744b0
matheuspf <matheuspff@gmail.com>
2017-10-31 13:44:13 -0300

    Add incremental fitting. Not working yet.

---
+++ IncrementalFitting/IncrementalFitting.cpp
@@ -0,0 +1,92 @@
+#include "IncrementalFitting.h"
+
+#include "../Preprocessing/Preprocess.h"
+
+#include "../Preprocessing/ModelSelection.h"
+
+#include "../Optimization/LineSearch/Backtracking/Backtracking.h"
+
+#include "../Optimization/CG/CG.h"
+
+#include "../Optimization/BFGS/BFGS.h"
+
+
+
+int main ()
+{
+    Mat X = readMat("../Data/Wine.txt", ',');
+
+    X = X.block(0, 0, 130, X.cols()-1);
+
+    Veci y = X.col(0).cast<int>();
+
+    X = X.block(0, 1, X.rows(), X.cols()-1);
+
+    //X = polyExpansion(X, 2, true);
+
+    auto [X_train, y_train, X_test, y_test] = trainTestSplit(X, y, 0.5, 1);
+
+
+    Standardize st;
+
+    X_train = st.fitTransform(X_train);
+    X_test = st.transform(X_test);
+
+
+    //using Optimizer = Newton<StrongWolfe, SimplyInvert<Eigen::LLT>>;
+    using Optimizer = BFGS<StrongWolfe, BFGS_Diagonal>;
+
+    Optimizer opt(StrongWolfe(1.0, 1e-2));
+
+    opt.maxIter = 10;
+    opt.gTol = 1e-3;
+
+
+    // using Optimizer = CG<HS, Backtracking>;
+    // Optimizer opt;
+
+    // opt.maxIterations = 10;
+    // opt.gTol = 1e-3;
+
+
+    IncrementalFitting<RBFBase, Optimizer> incf(opt);
+
+
+    // double runTime = benchmark([&]
+    // {
+        incf.fit(X_train, y_train);
+    // });
+
+    Veci y_pred = incf.predict(X_test);
+
+
+
+    // vector<double> alphas;
+
+    // for(double x = -5; x <= 5; x += 0.5)
+    //     alphas.pb(pow(10, x));
+
+    // auto gs = makeGridsearchCV(incf, make_pair([](auto& cls, double a){ cls.alpha = a; }, alphas), 5, Accuracy());
+
+    // gs.fit(X_train, y_train);
+
+    // incf = gs.bestEstimator;
+
+    // incf.fit(X_train, y_train);
+
+    // Veci y_pred = incf.predict(X_test);
+    
+
+
+    //db(incf.W, "\n\n", incf.intercept.transpose(), "\n\n", incf.alpha, "\n\n\n");
+
+    db(y_test.transpose(), "\n", y_pred.transpose(), "\n");
+
+    db((y_test.array() == y_pred.array()).cast<double>().sum() / y_test.rows(), "\n");
+
+    //db(runTime);
+
+
+
+    return 0;
+}
---
+++ IncrementalFitting/IncrementalFitting.h
@@ -0,0 +1,130 @@
+#ifndef CPP_ML_INCREMENTAL_FITTING_H
+#define CPP_ML_INCREMENTAL_FITTING_H
+
+#include "../Modelo.h"
+
+#include "../Classification/Classifier.h"
+
+#include "../Optimization/Newton/Newton.h"
+
+
+
+double logLikelihood (Vec x, const Veci& y)
+{
+    x = 1.0 / (1.0 + Eigen::exp(-x.array()));
+
+    return -(y.array().cast<double>() * log(x.array()) + 
+             (1.0 - y.array().cast<double>()) * log(1.0 - x.array())).sum();
+}
+
+
+struct RBFBase
+{
+};
+
+
+template <class Function = RBFBase, class Optimizer = Newton<Goldstein, CholeskyIdentity>>
+struct IncrementalFitting : public Classifier<IncrementalFitting<Function, Optimizer>>
+{
+    IncrementalFitting (const Function& function = Function(), const Optimizer& optimizer = Optimizer()) :
+                        function(function), optimizer(optimizer) {}
+
+    IncrementalFitting (const Optimizer& optimizer, const Function& function = Function()) :
+                        function(function), optimizer(optimizer) {}
+    
+
+    void fit_ (const Mat& X, const Veci& y, int K = 5, double minGap = 1e-5)
+    {
+        int M = X.rows(), N = X.cols();
+
+        alphas = Mat::Constant(K, N, 0.0);
+        gammas = Vec::Constant(K, 0.0);
+
+        weights = Vec::Constant(K, 0.0);
+        intercept = 0.0;
+
+        Vec predictions = Vec::Constant(M, 0.0);
+
+
+        for(int k = 0; k < K; ++k)
+        {
+            auto func = [&](Vec w) -> double
+            {
+                double w0 = w(N), wk = w(N+1), g = w(N+2);
+                
+                w.conservativeResize(N);
+
+                Vec activations = predictions.array() + w0 +
+                                  wk * Eigen::exp(-(X.rowwise() - w.transpose()).rowwise().squaredNorm().array() * g);
+                                  
+                return logLikelihood(activations, y);
+            };
+
+
+            RandDouble randDouble;
+
+
+            Vec w = Vec::NullaryExpr(N+3, [&](int){ return randDouble(0.1, 1.0); });
+
+            //w(N) = w(N+1) = w(N+2) = 1.0;
+
+            w = optimizer(func, w);
+
+
+            alphas.row(k) = w.head(N);
+
+            weights(k) = w(N);
+
+            intercept = w(N+1);
+
+            gammas(k) = w(N+2);
+
+
+            w.conservativeResize(N);
+
+            predictions = predictions.array() + weights(k) *
+                          Eigen::exp(-(X.rowwise() - w.transpose()).rowwise().squaredNorm().array() * gammas(k));
+
+            db("W:   ", w.transpose(), "\n\npred:   ", predictions.transpose(), "\n\ngamma:   ", gammas(k), 
+               "\n\nweight k:   ", weights(k), "\n\nintercept:   ", intercept, "\n\n\n\n");
+        }
+    }
+
+
+    int predict_ (Vec x)
+    {
+        x = Eigen::exp(-((alphas.rowwise() - x.transpose()).rowwise().squaredNorm().array() * gammas.array()));
+
+        return weights.dot(x) + intercept > 0;
+    }
+
+    Veci predict_ (const Mat& X)
+    {
+        Veci pred(X.rows());
+
+        for(int i = 0; i < X.rows(); ++i)
+            pred(i) = predict_(Vec(X.row(i)));
+
+        return pred;
+    }
+
+
+
+    Mat alphas;
+
+    Vec gammas;
+    
+
+    Vec weights;
+
+    double intercept;
+
+
+    Function function;
+    
+    Optimizer optimizer;
+};
+
+
+
+#endif // CPP_ML_INCREMENTAL_FITTING_H
